3be30af01718520be1506634c3de011c52395390
YG38 <ymebratu64@gmail.com>
2026-01-22 23:11:02 +0300

    Add PATCH method to CORS allowed methods
    - Fix CORS policy blocking PATCH requests
    - Allow admin to approve pharmacies via PATCH method
    - Resolves pharmacy approval network errors

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const corsOptions = {
     return callback(new Error('Not allowed by CORS'));
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'cache-control', 'pragma', 'cache-store', 'client-security-token']
 };
 
